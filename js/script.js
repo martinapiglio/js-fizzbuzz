6b3e690f6916ea5430579058242d6e982f96e96c
martinapiglio <martina.piglio@gmail.com>
2023-02-24 15:29:46 +0100

    js pseudo-code corr + if condition

--- js/script.js
+++ js/script.js
@@ -5,21 +5,21 @@
     b - condition to be verified is that i<=100;
     c - add a unit to variable i (i++);
 3 - each time the condition is verified, create a new element div inside the HTML;
-4 - add a class to this element so that it can be targeted inside the css
-5 - create an IF condition:
-    a - IF the variable 1 is a multiple of 3 ONLY, then:
+4 - add a class to this element so that it can be targeted inside the css;
+5 - append all the elements div inside the container
+6 - create an IF condition:
+    a - ELSE IF the variable 1 is a multiple of 3 AND a multiple of 5, then:
+        i - write "FizzBuzz";
+        ii - background-color is #f0466f;    
+    b - IF the variable 1 is a multiple of 3 ONLY, then:
         i - write "Fizz";
         ii - background-color is #0cd6a1;
-    b - ELSE IF the variable 1 is a multiple of 5 ONLY, then:
+    c - ELSE IF the variable 1 is a multiple of 5 ONLY, then:
         i - write "Buzz";
         ii - background-color is #ffd166;
-    c - ELSE IF the variable 1 is a multiple of 3 AND a multiple of 5, then:
-        i - write "FizzBuzz";
-        ii - background-color is #f0466f;
     d - ELSE:
         i - jut write the appropriate number;
         ii - background-color is #1389b2;
-6 - append all the elements div inside the container
 */
 
 //create a container variable
@@ -36,11 +36,35 @@ for (let i = 1; i <= 100; i++) {
     //create a new element div inside the HTML;
     let square = document.createElement('div');
 
-    //CHECK append the divs inside the containers 
+    //append all the elements div inside the container
     container.append(square);
-    //CHECK divs number is correct
+    
+    //CHECK
     console.log(i);
     //CHECK
+
+    //create an IF condition:
+    if (i % 3 == 0 && i % 5 == 0) {
+
+        square.append('fizzbuzz');
+        square.style.backgroundColor = "#f0466f"; 
+
+      } else if (i % 3 == 0) {
+
+        square.append('fizz');
+        square.style.backgroundColor = "#0cd6a1"; 
+
+      } else if (i % 5 == 0) {
+
+        square.append('buzz');
+        square.style.backgroundColor = "#ffd166"; 
+
+      } else {
+
+        square.append(i);
+        square.style.backgroundColor = "#1389b2"; 
+
+      }
 
 }
 
